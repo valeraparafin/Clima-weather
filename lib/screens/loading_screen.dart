6391d82bf537e2ce2278ad2683bed753baa25ddc
valeraparafin <iparafin@gmail.com>
2020-02-25 21:18:54 +0300

    completed Clima-Weather-App
    Whole app in one commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,41 @@
+import 'package:clima/services/weather.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import 'location_screen.dart';
+
+WeatherModel weatherModel = WeatherModel();
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  getLocationData() async {
+    var weatherData = await weatherModel.getLocationWeather();
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(locationWeather: weatherData);
+    }));
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    getLocationData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitPulse(
+          size: 350.0,
+          duration: Duration(milliseconds: 2000),
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
